c1af486e680ba7b9cac8feaf042f14fa2877fce5
Marcelo Pereira <marcelopereira@pop-os.localdomain>
2023-02-27 15:24:43 -0400

    Dia 06 - Atividades Assincronas

---
+++ 1 - Fundamentos/Dia06/parte 1.js
@@ -0,0 +1,6 @@
+let myName= "Marcelo";
+let birthCity= "Pedro Gomes";;
+let birthYear= "1992";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ 1 - Fundamentos/Dia06/parte 2.js
@@ -0,0 +1,9 @@
+let base=5;
+let heigth=8;
+let area=base*heigth;
+
+console.log(area);
+
+let perimeter=base+heigth+base+heigth;
+
+console.log(perimeter);
